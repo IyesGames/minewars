ef17003d071b56109c4865f8c8b465e68f7e430e
Ida Iyes <identry@protonmail.com>
2022-05-01 16:35:39 +0300

    add game mode and stream source states

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,52 @@ mod ui;
 
 pub const PROPRIETARY: bool = cfg!(feature = "proprietary");
 
+/// State type: If we are in-game, where is the gameplay data coming from?
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+enum StreamSource {
+    /// No active data source
+    Disconnected,
+    /// Network server
+    Connected,
+    /// We are the server (LAN)
+    Host,
+    /// Hosting for ourselves only (offline)
+    Local,
+    /// Replay file
+    File,
+}
+
+/// State type: If we are in-game, which mode are we in?
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+enum GameMode {
+    /// Not in game
+    None,
+    /// Playing multiplayer
+    /// (see also [`StreamSource`])
+    Multiplayer,
+    /// Watching (replays or live)
+    /// (see also [`StreamSource`])
+    Spectator,
+    /// Tutorial
+    Tutorial,
+    #[cfg(feature = "dev")]
+    Dev,
+}
+
+/// State type: Which "screen" is the app in?
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 enum AppGlobalState {
+    /// Initial loading screen at startup
     AssetsLoading,
+    /// Splash with the IyesGames logo
     SplashIyes,
+    /// Splash with the Bevy logo
     SplashBevy,
+    /// Main Menu
     MainMenu,
+    /// Gameplay
+    /// (see also [`GameMode`])
+    InGame,
 }
 
 fn main() {
@@ -52,6 +92,8 @@ fn main() {
         })
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugin(IyesEverything)
+        .add_loopless_state(StreamSource::Disconnected)
+        .add_loopless_state(GameMode::None)
         .add_loopless_state(AppGlobalState::AssetsLoading)
         .add_plugin(crate::ui::UiPlugin)
         .add_plugin(crate::assets::AssetsPlugin)
@@ -65,8 +107,12 @@ fn main() {
     app.run();
 }
 
-fn debug_current_state(state: Res<CurrentState<AppGlobalState>>) {
-    if state.is_changed() {
-        debug!("Detected state change to {:?}!", state);
+fn debug_current_state(
+    app: Res<CurrentState<AppGlobalState>>,
+    mode: Res<CurrentState<GameMode>>,
+    src: Res<CurrentState<StreamSource>>,
+) {
+    if app.is_changed() || mode.is_changed() || src.is_changed() {
+        debug!("State: {:?} / {:?} / {:?}!", app, mode, src);
     }
 }
