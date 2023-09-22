6e1ef5b42c58bfa264c116a2ced52dc285b06e91
Ida Iyes <identry@protonmail.com>
2023-09-22 20:00:42 +0300

    add SessionKind bevy app state

--- lib/mw_common/src/bevy.rs
+++ lib/mw_common/src/bevy.rs
@@ -19,6 +19,20 @@ pub enum AppState {
     InGame,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Default, States)]
+/// State type: What drives the game? Where do data and events come from?
+pub enum SessionKind {
+    /// Nowhere. We are not in a game session of any sort.
+    #[default]
+    Disconnected,
+    /// We are connected to a Host server. Network protocol drives the game.
+    NetHost,
+    /// We host/run our own gameplay in Bevy. The BevyHost drives the game.
+    BevyHost,
+    /// We are playing a replay file. We read data from it.
+    File,
+}
+
 /// Everything that must be despawned when transitioning the main app state
 #[derive(Component)]
 pub struct StateDespawnMarker;
@@ -37,6 +51,7 @@ pub struct MwCommonPlugin;
 impl Plugin for MwCommonPlugin {
     fn build(&self, app: &mut App) {
         app.add_state::<AppState>();
+        app.add_state::<SessionKind>();
         for state in enum_iterator::all::<AppState>() {
             app.add_systems(
                 OnExit(state),
