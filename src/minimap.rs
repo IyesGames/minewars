7bd63ba681a6b85c845b0dde904d804f09ba46b3
Ida Iyes <identry@protonmail.com>
2023-09-27 12:52:12 +0300

    oops minimap file was missing !!!!

---
+++ src/minimap.rs
@@ -0,0 +1,145 @@
+use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
+use mw_app::map::{MwTilePos, TileOwner, NeedsMapSet, MapUpdateSet};
+use mw_common::{game::{MapDescriptor, TileKind}, grid::Topology};
+
+use crate::{prelude::*, settings::NeedsSettingsSet};
+
+pub struct MinimapPlugin;
+
+impl Plugin for MinimapPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup_minimap);
+        app.add_systems(Update, (
+            update_minimap
+                .in_set(NeedsMapSet)
+                .in_set(NeedsSettingsSet)
+                .after(MapUpdateSet::TileOwner) // PERF ?
+                .run_if(resource_exists::<MinimapImage>()),
+        ));
+    }
+}
+
+#[derive(Resource)]
+pub struct MinimapImage(pub Handle<Image>);
+
+fn setup_minimap(
+    mut commands: Commands,
+    mut ass_image: ResMut<Assets<Image>>,
+) {
+    let image = Image::new_fill(Extent3d {
+        width: 256,
+        height: 256,
+        depth_or_array_layers: 1,
+    }, TextureDimension::D2, &[0, 0, 0, 0], TextureFormat::Rgba8UnormSrgb);
+    let handle = ass_image.add(image);
+    commands.insert_resource(MinimapImage(handle));
+}
+
+fn update_minimap(
+    settings: Res<AllSettings>,
+    minimap_handle: Res<MinimapImage>,
+    mut ass_image: ResMut<Assets<Image>>,
+    desc: Res<MapDescriptor>,
+    q_tile: Query<
+        (&MwTilePos, Option<&TileOwner>, &TileKind),
+        Or<(Changed<TileOwner>, Changed<TileKind>)>
+    >,
+) {
+    if desc.is_changed() || settings.is_changed() {
+        let minimap_image = ass_image.get_mut(&minimap_handle.0)
+            .expect("minimap image must exist");
+
+        let span = desc.size as u32 * 2 + 1;
+        let (height, width) = match desc.topology {
+            Topology::Hex => {
+                let tile_size = settings.ui_hud.minimap_scale.max(2) as u32;
+                let y_extra = tile_size / 2;
+                (
+                   span * (tile_size + y_extra * 2) + tile_size + 1,
+                   span * tile_size * 2,
+                )
+            },
+            Topology::Sq => {
+                let tile_size = settings.ui_hud.minimap_scale.max(2) as u32 - 1;
+                (
+                   span * (tile_size * 2 + 1),
+                   span * (tile_size * 2 + 1),
+                )
+            },
+        };
+        *minimap_image = Image::new_fill(
+            Extent3d {
+                width, height, depth_or_array_layers: 1,
+            },
+            TextureDimension::D2,
+            &[0, 0, 0, 0],
+            TextureFormat::Rgba8UnormSrgb,
+        );
+    }
+
+    for (pos, owner, kind) in &q_tile {
+        let minimap_image = ass_image.get_mut(&minimap_handle.0)
+            .expect("minimap image must exist");
+        let w = minimap_image.texture_descriptor.size.width as i32;
+        let h = minimap_image.texture_descriptor.size.height as i32;
+
+        let rgba = owner
+            .map(|o| Color::from(settings.player_colors.visible[o.0.i()]).as_rgba_u8())
+            .unwrap_or([0, 0, 0, 0]);
+        match desc.topology {
+            Topology::Hex => {
+                let tile_size = settings.ui_hud.minimap_scale.max(2) as i32;
+                let y_extra = tile_size / 2;
+                let cy = pos.0.0 as i32;
+                let cx = pos.0.1 as i32;
+                let y = h / 2 - cy * (tile_size + y_extra * 2);
+                let x = w / 2 + cx * (tile_size * 2) + cy * tile_size;
+                for yy in (y-(tile_size+y_extra))..=(y+(tile_size+y_extra)) {
+                    let t = ((yy - y).abs()-y_extra).max(0);
+                    for xx in (x-(tile_size-t))..(x+(tile_size-t)) {
+                        if yy < 0 || xx < 0 || yy >= h || xx >= w {
+                            continue;
+                        }
+                        let i = (yy * w + xx) as usize * 4;
+                        minimap_image.data[i..(i+4)].copy_from_slice(&rgba);
+                    }
+                }
+                match kind {
+                    TileKind::Mountain => {
+                        let rgba = [0, 0, 0, 255];
+                        let i = (y * w + x) as usize * 4;
+                        minimap_image.data[i..(i+4)].copy_from_slice(&rgba);
+                        let i = (y * w + x - 1) as usize * 4;
+                        minimap_image.data[i..(i+4)].copy_from_slice(&rgba);
+                        let i = (y * w + x + 1) as usize * 4;
+                        minimap_image.data[i..(i+4)].copy_from_slice(&rgba);
+                        let i = ((y - 1) * w + x) as usize * 4;
+                        minimap_image.data[i..(i+4)].copy_from_slice(&rgba);
+                    }
+                    TileKind::Forest => {
+                        let rgba = [0, 0, 0, 255];
+                        let i = ((y - 1) * w + x - 1) as usize * 4;
+                        minimap_image.data[i..(i+4)].copy_from_slice(&rgba);
+                        let i = ((y + 1) * w + x + 1) as usize * 4;
+                        minimap_image.data[i..(i+4)].copy_from_slice(&rgba);
+                    }
+                    _ => {}
+                }
+            }
+            Topology::Sq => {
+                let tile_size = settings.ui_hud.minimap_scale.max(2) as i32 - 1;
+                let y = h / 2 - pos.0.0 as i32 * (tile_size * 2 + 1);
+                let x = w / 2 + pos.0.1 as i32 * (tile_size * 2 + 1);
+                for yy in (y-tile_size)..=(y+tile_size) {
+                    for xx in (x-tile_size)..=(x+tile_size) {
+                        if yy < 0 || xx < 0 || yy >= h || xx >= w {
+                            continue;
+                        }
+                        let i = (yy * w + xx) as usize * 4;
+                        minimap_image.data[i..(i+4)].copy_from_slice(&rgba);
+                    }
+                }
+            }
+        }
+    }
+}
