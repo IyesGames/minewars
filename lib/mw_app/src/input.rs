e0329a180f3ff7426e8b36ca4b6b3d7f5fb6b368
Ida Iyes <identry@protonmail.com>
2024-04-28 15:27:09 +0300

    Detect what input devices are in use

--- lib/mw_app/src/input.rs
+++ lib/mw_app/src/input.rs
@@ -1,3 +1,6 @@
+use bevy::input::gamepad::GamepadEvent;
+use bevy::input::keyboard::KeyboardInput;
+use bevy::input::mouse::MouseMotion;
 use mw_common::grid::{Hex, Sq};
 
 use crate::prelude::*;
@@ -11,18 +14,34 @@ mod touch;
 
 pub fn plugin(app: &mut App) {
     app.add_event::<InputAction>();
+    app.init_resource::<DetectedInputDevices>();
     app.init_resource::<CurrentTool>();
     app.init_resource::<ActiveAnalogs>();
     app.configure_sets(Update, (
+        GameInputSet::Detect,
         GameInputSet::Collect
             .in_set(InStateSet(AppState::InGame))
-            .run_if(rc_accepting_game_input),
+            .run_if(rc_accepting_game_input)
+            .after(GameInputSet::Detect),
         GameInputSet::Process
             .in_set(InStateSet(AppState::InGame))
             .after(GameInputSet::Collect),
         GameInputSet::ProcessEvents
+            .in_set(InStateSet(AppState::InGame))
             .in_set(GameInputSet::Process)
             .run_if(on_event::<InputAction>()),
+        InputDeviceEnabledSet::Kbd
+            .after(GameInputSet::Detect)
+            .run_if(rc_input_device(InputDeviceEnabledSet::Kbd)),
+        InputDeviceEnabledSet::Mouse
+            .after(GameInputSet::Detect)
+            .run_if(rc_input_device(InputDeviceEnabledSet::Mouse)),
+        InputDeviceEnabledSet::Touch
+            .after(GameInputSet::Detect)
+            .run_if(rc_input_device(InputDeviceEnabledSet::Touch)),
+        InputDeviceEnabledSet::Gamepad
+            .after(GameInputSet::Detect)
+            .run_if(rc_input_device(InputDeviceEnabledSet::Gamepad)),
     ));
     app.add_plugins((
         gamepad::plugin,
@@ -32,15 +51,37 @@ pub fn plugin(app: &mut App) {
     ));
     app.add_systems(OnEnter(AppState::InGame), clear_game_input);
     app.add_systems(Update, (
-        input_tool_event.in_set(GameInputSet::ProcessEvents).before(ToolEventHandlerSet),
+        detect_input_devices
+            .run_if(rc_detect_input_devices)
+            .in_set(GameInputSet::Detect),
+        input_tool_event
+            .in_set(GameInputSet::ProcessEvents)
+            .before(ToolEventHandlerSet),
     ));
 }
 
 #[derive(SystemSet, Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum GameInputSet {
+    Detect,
     Collect,
     Process,
     ProcessEvents,
+}
+
+#[derive(SystemSet, Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum InputDeviceEnabledSet {
+    Kbd,
+    Mouse,
+    Touch,
+    Gamepad,
+}
+
+#[derive(Resource, Default)]
+pub struct DetectedInputDevices {
+    pub kbd: bool,
+    pub mouse: bool,
+    pub touch: bool,
+    pub gamepad: bool,
 }
 
 #[derive(Event, PartialEq, Clone, Copy, Debug)]
@@ -102,6 +143,15 @@ impl AnalogSource {
 #[derive(Resource, Default)]
 struct ActiveAnalogs(HashMap<AnalogInput, AnalogSource>);
 
+fn rc_input_device(device: InputDeviceEnabledSet) -> impl Fn(Res<DetectedInputDevices>) -> bool {
+    match device {
+        InputDeviceEnabledSet::Kbd => |d: Res<DetectedInputDevices>| d.kbd,
+        InputDeviceEnabledSet::Mouse => |d: Res<DetectedInputDevices>| d.mouse,
+        InputDeviceEnabledSet::Touch => |d: Res<DetectedInputDevices>| d.touch,
+        InputDeviceEnabledSet::Gamepad => |d: Res<DetectedInputDevices>| d.gamepad,
+    }
+}
+
 fn activate_input(
     input: InputAction,
     analog_source: AnalogSource,
@@ -147,6 +197,40 @@ fn clear_game_input(
 ) {
     ev.clear();
     analogs.0.clear();
+}
+
+fn rc_detect_input_devices(
+    evr_mouse: Res<Events<MouseMotion>>,
+    evr_kbd: Res<Events<KeyboardInput>>,
+    evr_touch: Res<Events<TouchInput>>,
+    evr_gamepad: Res<Events<GamepadEvent>>,
+) -> bool {
+    !evr_mouse.is_empty() ||
+    !evr_kbd.is_empty() ||
+    !evr_touch.is_empty() ||
+    !evr_gamepad.is_empty()
+}
+
+fn detect_input_devices(
+    evr_mouse: Res<Events<MouseMotion>>,
+    evr_kbd: Res<Events<KeyboardInput>>,
+    evr_touch: Res<Events<TouchInput>>,
+    evr_gamepad: Res<Events<GamepadEvent>>,
+    gamepads: Res<Gamepads>,
+    mut detected: ResMut<DetectedInputDevices>,
+) {
+    if !detected.mouse && !evr_mouse.is_empty() {
+        detected.mouse = true;
+    }
+    if !detected.kbd && !evr_kbd.is_empty() {
+        detected.kbd = true;
+    }
+    if !detected.touch && !evr_touch.is_empty() {
+        detected.touch = true;
+    }
+    if !evr_gamepad.is_empty() {
+        detected.gamepad = gamepads.iter().count() != 0;
+    }
 }
 
 fn input_tool_event(
