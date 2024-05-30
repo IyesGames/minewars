c1bc141706ecd2d6e4a53bf3942f91d666ba94e6
Ida Iyes <identry@protonmail.com>
2024-05-30 23:42:25 +0300

    add some cli commands for testing app state transitions

--- lib/app/mw_app/src/cli.rs
+++ lib/app/mw_app/src/cli.rs
@@ -1,4 +1,27 @@
+use bevy::app::AppExit;
+
 use crate::prelude::*;
 
 pub fn plugin(app: &mut App) {
+    app.register_clicommand_noargs("softreset", softreset);
+    app.register_clicommand_noargs("exit_game", exit_game);
+    app.register_clicommand_noargs("exit_app", exit_app);
+}
+
+fn softreset(
+    mut s: ResMut<NextState<AppState>>,
+) {
+    s.set(AppState::GameLoading);
+}
+
+fn exit_game(
+    mut s: ResMut<NextState<AppState>>,
+) {
+    s.set(AppState::Menu);
+}
+
+fn exit_app(
+    mut evw: EventWriter<AppExit>,
+) {
+    evw.send(AppExit);
 }
